aa5871d7b51c9307626759fdf8cdfb9e0c82cd1b
Riccardo Rizzo <riccardo.rizzo@machinemax.com>
2022-01-22 17:19:00 +0000

    homeModule blacklisted for redux-persist

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -13,6 +13,7 @@ import storage from "redux-persist/lib/storage"; // defaults to localStorage for
 const persistConfig = {
   key: "root",
   storage,
+  blacklist: ["homeModule"],
 };
 
 export const composeEnhancers =
